0fb09addc97f1d139b07ad224386b6bb626680dc
Lucas <freirejandre@gmail.com>
2019-09-20 01:10:02 -0300

    fdsfds

---
+++ app/Http/Middleware/VerifyToken.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Carbon\Carbon;
+
+class VerifyToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request->user()){
+            if (Carbon::parse($request->user()->token()->expires_at) < Carbon::now()) {
+                $request->user()->token()->revoke();
+                $request->user()->token()->delete();
+                return response()->json(['error'], 401);
+            }
+        }
+
+        return $next($request);
+    }
+}
