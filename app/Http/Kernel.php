73cae673b07e2e36b7934fc4b9287ae04b0b2542
Lucas <freirejandre@gmail.com>
2019-09-22 19:02:11 -0300

    dasdsad

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            'verifytoken'
         ],
     ];
 
@@ -61,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'verifytoken'=> \App\Http\Middleware\VerifyToken::class,
     ];
 
     /**
