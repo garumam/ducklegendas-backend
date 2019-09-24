a6c648bc474b25cb09664b9facb780f120a7d486
Felipe Ribeiro <felipe.epr.faculdade@gmail.com>
2019-09-23 22:30:16 -0300

    erro de rota

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,8 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            'bindings',
-            'verifytoken'
+            'bindings'
         ],
     ];
 
@@ -62,7 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'verifytoken'=> \App\Http\Middleware\VerifyToken::class,
+        //'verifytoken'=> \App\Http\Middleware\VerifyToken::class,
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api','verifytoken')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('erro', function (){
+    return response()->json(['error' => 'unauthenticated'], 401);
+})->name('login');
 
 Route::group(['namespace' => 'Api'],function () {
     Route::post('login', 'Auth\UserController@login');
