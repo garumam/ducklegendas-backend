115a98ab5153b6a7973b512e357be46ba9e0c9aa
Felipe Ribeiro <felipe.epr.faculdade@gmail.com>
2019-10-21 19:39:03 -0300

    ajuste pesquisa ranking

--- app/Http/Controllers/Api/RankingController.php
+++ app/Http/Controllers/Api/RankingController.php
@@ -16,14 +16,75 @@ class RankingController extends Controller
         if(Gate::denies('isAdmin')){
             return response()->json(['error'=> ['Acesso negado para este conteúdo!']], $this->errorStatus);
         }
+        $all = User::withCount('subtitles')->orderBy('subtitles_count','desc')->get();
         
-        $query = User::withCount('subtitles')->where('name','like', '%'.$request->search.'%')->orderBy('subtitles_count','desc');
-        $user = $query->paginate(100);
-        $user->data = $user->each(function ($item, $key) use ($request) {
-            $page = $request->has('page')? $request->page : 1;
-            $item->id = ($key + 1) + ($page - 1) * 100;
+        $allArray = $all->toArray();
+        $position = 1;
+        $count = $allArray[0]['subtitles_count'];
+        foreach($allArray as &$value){
+            if($value['subtitles_count'] < $count){
+                $count = $value['subtitles_count'];
+                $position++;
+            }
+            $value = array_merge($value,['position' => $position]);
+        }
+        unset($value);
+
+        $filteredCollect = $all->filter(function($item) use ($request){
+            if(!empty($request->search)){
+                return strpos(
+                    strtolower($item['name']), 
+                    strtolower($request->search)
+                ) === false? false : true;
+            }
+            return true;
         });
-        return response()->json(['success'=>$user], $this->successStatus);
+
+        $filteredCollect = $filteredCollect->each(function ($item) use ($allArray) {
+            foreach($allArray as $value){
+                if($item['id'] === $value['id'])
+                    $item['id'] = $value['position'];
+            }
+        });
+
+        $filteredCollect = $filteredCollect->toArray();
+        $page = $request->has('page')? $request->page : 1;
+        $initialPosition = ($page - 1) * 100;
+        $filteredCount = sizeof($filteredCollect);
+        $filteredCollect = array_slice($filteredCollect, $initialPosition, 100);
+
+        return response()->json(['success'=>[
+            'data'=>$filteredCollect, 
+            'total'=>$filteredCount
+        ]], $this->successStatus);
     }
+
+    // public function getAll(Request $request){
+    //     if(Gate::denies('isAdmin')){
+    //         return response()->json(['error'=> ['Acesso negado para este conteúdo!']], $this->errorStatus);
+    //     }
+    //     $all = User::withCount('subtitles')->orderBy('subtitles_count','desc')->get();
+    //     $query = User::withCount('subtitles')->where('name','like', '%'.$request->search.'%')->orderBy('subtitles_count','desc');
+    //     $user = $query->paginate(100);
+    //     $allArray = $all->toArray();
+    //     $position = 1;
+    //     $count = $allArray[0]['subtitles_count'];
+    //     foreach($allArray as &$value){
+    //         if($value['subtitles_count'] < $count){
+    //             $count = $value['subtitles_count'];
+    //             $position++;
+    //         }
+    //         $value = array_merge($value,['position' => $position]);
+    //     }
+    //     unset($value);
+
+    //     $user->data = $user->each(function ($item, $key) use ($allArray) {
+    //         foreach($allArray as $value){
+    //             if($item->id === $value['id'])
+    //                 $item->id = $value['position'];
+    //         }
+    //     });
+    //     return response()->json(['success'=>$user], $this->successStatus);
+    // }
 
 }
