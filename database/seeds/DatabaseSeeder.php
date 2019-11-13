aa5ec3d39b481a1f7e1ce107c9359ad6dcd68f3e
Felipe Ribeiro <felipe.epr.faculdade@gmail.com>
2019-11-12 22:03:29 -0300

    seed de categoria

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,31 +16,56 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        factory(App\User::class, 250)->create();
+        //factory(App\User::class, 250)->create();
 
-        $category = Category::create([
-            'name' => 'Comédia'
-        ]);
+        $categories = array(
+            'Ação',
+            'Animação',
+            'Aventura',
+            'Biografia',
+            'Comédia',
+            'Crime',
+            'Documentário',
+            'Drama',
+            'Esporte',
+            'Família',
+            'Fantasia',
+            'Faroeste',
+            'Ficção',
+            'Guerra',
+            'História',
+            'Mistério',
+            'Musical',
+            'Policial',
+            'Romance',
+            'Suspense',
+            'Terror'
+        );
+        foreach($categories as $category){
+            Category::create([
+                'name' => $category
+            ]);
+        }
+        
+        // Subtitle::create([
+        //     'name' => 'Stranger things',
+        //     'year' => 2019,
+        //     'url' => 'https://www.google.com/',
+        //     'episode' => 's03e03',
+        //     'type' => 'SERIE',
+        //     'image' => '',
+        //     'status' => 'APROVADA',
+        //     'author' => 1,
+        //     'category' => $category->id
+        // ]);
 
-        Subtitle::create([
-            'name' => 'Stranger things',
-            'year' => 2019,
-            'url' => 'https://www.google.com/',
-            'episode' => 's03e03',
-            'type' => 'SERIE',
-            'image' => '',
-            'status' => 'APROVADA',
-            'author' => 1,
-            'category' => $category->id
-        ]);
+        // factory(App\Subtitle::class, 100)->create();
 
-        factory(App\Subtitle::class, 100)->create();
-
-        SubtitleProgress::create([
-            'name' => 'Serie em andamento',
-            'percent' => 80,
-            'author' => 1
-        ]);
+        // SubtitleProgress::create([
+        //     'name' => 'Serie em andamento',
+        //     'percent' => 80,
+        //     'author' => 1
+        // ]);
             
         // Gallery::create([
         //     'name' => 'peaky_blinders',
